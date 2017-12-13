83c05de52b82cf0059f63a0f4ffc2d196b1dcb89
jay <gaoljie@gmail.com>
2017-12-13 21:59:09 +0800

    dsa

--- cloud/main.js
+++ cloud/main.js
@@ -6,27 +6,33 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('sendVerifySMS', function(req, res) {
-    let phone = req.params.phone
+    console.log(req.params)
+    var phone = req.params.phone
+    console.log(phone)
+    console.log(process.env.BMOB_APP_ID)
+    console.log(process.env.BMOB_API_KEY)
+
     axios({
-      method: 'post',
-      url: 'https://api.bmob.cn/1/requestSmsCode',
-      data: {
-          mobilePhoneNumber: phone,
-          template: 'szamigo'
-      },
-      headers: {
-          'X-Bmob-Application-Id': process.env.BMOB_APP_ID,
-          'X-Bmob-REST-API-Key': process.env.BMOB_API_KEY,
-          'Content-Type': 'application/json'
-      }
+        method: 'post',
+        url: 'https://api.bmob.cn/1/requestSmsCode',
+        data: {
+            mobilePhoneNumber: phone,
+            template: 'szamigo'
+        },
+        headers: {
+            'X-Bmob-Application-Id': process.env.BMOB_APP_ID,
+            'X-Bmob-REST-API-Key': process.env.BMOB_API_KEY,
+            'Content-Type': 'application/json'
+        }
     }).then((result) => {
-      res.success(result)
+        console.log(result)
+        res.success(result)
     }).catch((err) =>  {
         res.error(err);
     });
 });
 
-Parse.Cloud.define('sendVerifySMS', function(req, res) {
+Parse.Cloud.define('verifySMS', function(req, res) {
     let phone = req.params.phone
     let code = req.params.code
     axios({
