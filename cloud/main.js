7c09beb08123f21869da2c6d84adb90082807cd5
jay <gaoljie@gmail.com>
2017-12-13 20:36:28 +0800

    sms

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,43 @@
+import axios from 'axios'
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('sendVerifySMS', function(req, res) {
+    let phone = req.params.phone
+    axios({
+      method: 'post',
+      url: 'https://api.bmob.cn/1/requestSmsCode',
+      data: {
+          mobilePhoneNumber: phone,
+          template: 'szamigo'
+      },
+      headers: {
+          'X-Bmob-Application-Id': process.env.BMOB_APP_ID,
+          'X-Bmob-REST-API-Key': process.env.BMOB_API_KEY,
+          'Content-Type': 'application/json'
+      }
+    }).then((result) => {
+      res.success(result)
+    }).catch((err) =>  {
+        response.error(err);
+    });
+});
+
+Parse.Cloud.define('sendVerifySMS', function(req, res) {
+    let phone = req.params.phone
+    let code = req.params.code
+    axios({
+        method: 'post',
+        url: 'https://api.bmob.cn/1/verifySmsCode/' + code,
+        data: {
+            mobilePhoneNumber: phone
+        },
+        headers: {
+            'X-Bmob-Application-Id': process.env.BMOB_APP_ID,
+            'X-Bmob-REST-API-Key': process.env.BMOB_API_KEY,
+            'Content-Type': 'application/json'
+        }
+    }).then((result) => {
+        res.success(result)
+    }).catch((err) =>  {
+        response.error(err);
+    });
 });
